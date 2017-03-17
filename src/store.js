961828867e740098d0b336d7a2d39f67192367e2
Kimak <mickael.dumand@gmail.com>
2017-03-17 15:58:57 +0100

    add redux devtools

--- src/store.js
+++ src/store.js
@@ -2,6 +2,9 @@ import { combineReducers, createStore } from 'redux';
 import todos from './reducers/todos';
 
 let reducer = combineReducers({ todos });
-let store = createStore(reducer);
+let store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store
